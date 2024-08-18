78fa2a5ce915e94019ba73cf1b25f84280e5fed6
Ephy Kimotho <ephyk48@gmail.com>
2024-08-18 23:07:03 +0300

    Stlyed Footer, Main and Header components

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer>
+    <footer style={{backgroundColor:"slateblue", padding:".75rem", fontSize:"1.125rem"}}>
       <p>© 2023 City Lovers</p>
     </footer>
   );
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
   return (
-    <main>
+    <main style={{backgroundColor:"magenta", color:"#f3f2f2", padding:"10px"}}>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );

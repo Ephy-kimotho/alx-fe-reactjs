74e926ea31e77b8bf85c61e5aea9dc1209d0e611
Ephy Kimotho <ephyk48@gmail.com>
2024-08-19 15:26:54 +0300

    Update userProfile to use Context API

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/prop-types */
+import UserContext from "./UserContext";
+import { useContext } from "react";
+
 function UserProfile(props) {
+  const userData = useContext(UserContext)
   return (
     <div>
-      <h2>{props.name}</h2>
+      <h2>{userData.name}</h2>
       <p>Age: {props.age}</p>
       <p>Bio: {props.bio}</p>
     </div>

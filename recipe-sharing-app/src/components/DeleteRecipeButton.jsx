0e9fae88194eb24365e25771c13dd4f0b1db7612
Ephy Kimotho <ephyk48@gmail.com>
2024-08-28 12:52:32 +0300

    added redirection to home page when a recipe is deleted

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/prop-types */
 import useRecipeStore from "./recipeStore"
+import { useNavigate } from "react-router-dom"
 
 function DeleteRecipeButton({ recipeId }) {
     const deleteRecipe = useRecipeStore(state => state.deleteRecipe)
+    const navigate = useNavigate()
+
+    function handleDelete(){
+      deleteRecipe(recipeId)
+
+      setTimeout(() => {
+        navigate("/")
+      }, 1000)
+    }
 
   return (
-    <button onClick={() => deleteRecipe(recipeId)} className="delete-btn">
+    <button onClick={handleDelete} className="delete-btn">
       Delete Recipe
     </button>
   )

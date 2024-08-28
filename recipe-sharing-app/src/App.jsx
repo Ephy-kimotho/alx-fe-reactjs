97b929b12735d093fe29b7a5ce7eb154d30367c5
Ephy Kimotho <ephyk48@gmail.com>
2024-08-28 13:03:27 +0300

    used BrowserRouter as Router

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
@@ -6,14 +6,16 @@ import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
-    <main>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/add-recipe" element={<AddRecipeForm />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
-    </main>
+    <Router>
+      <main>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add-recipe" element={<AddRecipeForm />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 

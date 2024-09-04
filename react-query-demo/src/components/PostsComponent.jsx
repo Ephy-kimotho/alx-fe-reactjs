4d2b380c1341123738ee8d4264433d9a61257fe8
Ephy Kimotho <ephyk48@gmail.com>
2024-09-04 11:50:27 +0300

    Built a todo list using react query

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,50 @@
+import { useQuery } from "react-query";
+
+async function getTodos(url) {
+  const res = await fetch(url);
+  return await res.json();
+}
+
+function PostsComponent() {
+  const { data, isError, isLoading, refetch } = useQuery({
+    queryKey: ["todos"],
+    queryFn: () => getTodos("https://jsonplaceholder.typicode.com/posts"),
+    staleTime: 3000,
+    gcTime: 6000,
+  });
+
+  if (isError) {
+    return (
+      <div className="error">
+        <h1>There was an error!!</h1>
+        <button onClick={() => refetch()}>Try Again</button>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Data is loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <section>
+      <button onClick={() => refetch()} className="refetch-btn">
+        Refetch Posts
+      </button>
+      <div>
+        {data.slice(0, 10).map((todo) => (
+          <article key={todo.id} className="post">
+            <h2>{todo.title}</h2>
+            <p>{todo.body}</p>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default PostsComponent;

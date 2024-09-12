38c6baeb623d653b14557b30db75cb3eae49b094
Ephy Kimotho <ephyk48@gmail.com>
2024-09-12 15:14:52 +0300

    Made the card responsive

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,14 +2,16 @@ import Carlos from "../assets/carlos.jpg";
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src={Carlos}
         alt="User"
-        className="w-36 h-36  rounded-full mx-auto"
+        className="sm:w-24 sm:h-24 md:w-36 md:h-36  rounded-full mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4 font-bold">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 font-bold">
+        John Doe
+      </h1>
+      <p className="text-gray-600 sm:text-sm  md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>

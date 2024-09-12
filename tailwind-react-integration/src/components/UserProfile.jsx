9d756029bd38e11772b7a85b18b8e2b86181b09d
Ephy Kimotho <ephyk48@gmail.com>
2024-09-12 15:42:19 +0300

    Add interactivity effects on card

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,13 +2,13 @@ import Carlos from "../assets/carlos.jpg";
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
       <img
         src={Carlos}
         alt="User"
-        className="sm:w-24 sm:h-24 md:w-36 md:h-36  rounded-full mx-auto"
+        className="sm:w-24 sm:h-24 md:w-36 md:h-36  rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 font-bold">
+      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 font-bold hover:text-blue-500">
         John Doe
       </h1>
       <p className="text-gray-600 sm:text-sm  md:text-base">

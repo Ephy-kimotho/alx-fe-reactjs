82a2127cc6b99e45c5af0220355a1337b8185533
Ephy Kimotho <ephyk48@gmail.com>
2024-09-15 20:31:42 +0300

    Added a div to main heading

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -21,9 +21,11 @@ function HomePage() {
 
   return (
     <section className="bg-gray pt-6 px-9 pb-9">
-      <h1 className="text-3xl sm:text-4xl font-bold text-center text-red uppercase my-6">
-        Welcome to MapishiHub
-      </h1>
+      <div className="bg-blue rounded shadow hover:shadow-md">
+        <h1 className="text-3xl sm:text-4xl font-bold text-center text-gray py-1 uppercase my-6">
+          Welcome to MapishiHub
+        </h1>
+      </div>
       <div className="bg-gray grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {recipeElements}
       </div>

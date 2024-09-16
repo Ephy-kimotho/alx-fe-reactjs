1d35ba2949314990c587eefd8c5fd5e39f6a2769
Ephy Kimotho <ephyk48@gmail.com>
2024-09-16 18:33:04 +0300

    Implemented form validation

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -8,16 +8,80 @@ function AddRecipeForm() {
     steps: "",
   });
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  const [errors, setErrors] = useState({
+    title: "",
+    description: "",
+    ingredients: "",
+    steps: "",
+  });
 
+  function validate() {
+    const { title, description, ingredients, steps } = formData;
+
+    if (title.length === 0) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        title: "Required",
+      }));
+    } else if (title.length < 5) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        title: "Too short",
+      }));
+    }
+
+    if (description.length === 0) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        description: "Required",
+      }));
+    } else if (description.length < 5) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        description: "Too short",
+      }));
+    }
+
+    if (ingredients.length === 0) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        ingredients: "Required",
+      }));
+    } else if (ingredients.split(",").length < 2) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        ingredients: "Enter two or more ingredients",
+      }));
+    }
+
+    if (steps.length === 0) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        steps: "Required",
+      }));
+    } else if (steps.split(",").length < 2) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        steps: "Enter two or more steps",
+      }));
+    }
+  }
+
+  function handleChange(e) {
     const { name, value } = e.target.value;
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
     }));
+  }
 
-    console.log(formData);
+  //function postData(){}
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    validate();
+    console.log("Form submitted");
   }
 
   return (
@@ -27,7 +91,14 @@ function AddRecipeForm() {
 
         <div>
           <label htmlFor="title">Title: </label>
-          <input type="text" name="title" placeholder="Enter the title" />
+          <input
+            type="text"
+            name="title"
+            placeholder="Enter the title"
+            value={formData.title}
+            onChange={handleChange}
+          />
+          {errors.title && <p className="text-red">{errors.title}</p>}
         </div>
 
         <div>
@@ -36,7 +107,12 @@ function AddRecipeForm() {
             type="text"
             name="description"
             placeholder="Enter the description"
+            value={formData.description}
+            onChange={handleChange}
           />
+          {errors.description && (
+            <p className="text-red">{errors.description}</p>
+          )}
         </div>
 
         <div>
@@ -44,15 +120,23 @@ function AddRecipeForm() {
           <textarea
             name="ingredients"
             placeholder="Enter ingredients separated by a comma"
+            value={formData.ingredients}
+            onChange={handleChange}
           />
+          {errors.ingredients && (
+            <p className="text-red">{errors.ingredients}</p>
+          )}
         </div>
 
         <div>
           <label htmlFor="steps">Preparation steps:</label>
           <textarea
             name="steps"
-            placeholder="Enter preparation stepsseparated by a comma"
+            placeholder="Enter preparation steps separated by a comma"
+            value={formData.steps}
+            onChange={handleChange}
           />
+          {errors.steps && <p className="text-red">{errors.steps}</p>}
         </div>
 
         <div>

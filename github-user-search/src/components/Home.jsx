d61b22e4148bd6bc1f446374dc5ae51157009164
Ephy Kimotho <ephyk48@gmail.com>
2024-09-22 21:09:59 +0300

    chore: created Home and service components

---
+++ github-user-search/src/components/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div>
+      Home component goes here
+    </div>
+  )
+}
+
+export default Home
---
+++ github-user-search/src/services/service.jsx
@@ -0,0 +1,11 @@
+
+
+function service() {
+  return (
+    <div>
+      service component goes here
+    </div>
+  )
+}
+
+export default service

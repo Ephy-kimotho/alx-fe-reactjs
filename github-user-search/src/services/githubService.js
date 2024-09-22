ae44ac7c52e4a4de2eaf1f222a3b113d12694464
Ephy Kimotho <ephyk48@gmail.com>
2024-09-22 23:45:33 +0300

    refactor: explicitly used get method in githubService file

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 async function fetchUserData(username) {
   const url = `https://api.github.com/users/${username}`;
   try {
-    const response = await axios(url);
+    const response = await axios.get(url);
     return response;
   } catch (error) {
     return error.response;
